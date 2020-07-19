49080c571d9b1c276ef8d0e4af7eb8c1ef5608e9
Alexandra Bashkirova <s.bshkrva@gmail.com>
2020-07-19 13:29:58 +0700

    Add volume settings to player

--- WhiteNoise-SwiftUI/Services/Player/NoisePlayer.swift
+++ WhiteNoise-SwiftUI/Services/Player/NoisePlayer.swift
@@ -11,11 +11,29 @@ import AVFoundation
 
 class NoisePlayer {
     private var player: AVAudioPlayer?
+    var currentVolume: Float = 0.5 {
+        didSet {
+            player?.volume = currentVolume
+        }
+    }
     var currentAudio: AudioData?
     var isPlaying: Bool {
         player?.isPlaying ?? false
     }
-    
+
+    private func makePlayer(audioTrack: AudioData) -> AVAudioPlayer? {
+        let url = Bundle.main.url(
+            forResource: audioTrack.name,
+            withExtension: "mp3")!
+        let player = try? AVAudioPlayer(contentsOf: url)
+        player?.numberOfLoops = -1
+        player?.volume = currentVolume
+        return player
+    }
+}
+
+// MARK: player
+extension NoisePlayer {
     func setAudio(_ audio: AudioData) {
         currentAudio = audio
         player = makePlayer(audioTrack: audio)
@@ -28,13 +46,11 @@ class NoisePlayer {
     func stop() {
         player?.pause()
     }
-    
-    private func makePlayer(audioTrack: AudioData) -> AVAudioPlayer? {
-        let url = Bundle.main.url(
-            forResource: audioTrack.name,
-            withExtension: "mp3")!
-        let player = try? AVAudioPlayer(contentsOf: url)
-        player?.numberOfLoops = -1
-        return player
+}
+
+// MARK: volume
+extension NoisePlayer {
+    func setVolume(_ volume: Double) {
+        currentVolume = Float(volume)
     }
 }
